23771ec6c0edbe1644b2a78cc2590bea398c74a3
Sarah <sarahestrada33@gmail.com>
2026-05-31 19:26:40 -0600

    terminando de instalar dependencias

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,30 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import globals from 'globals'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
+export default [js.configs.recommended, {
+  files: ['src/**/*.{js,jsx,ts,tsx}'],
+  plugins: {
+    react: reactPlugin,
+    'react-hooks': reactHooks,
+  },
+  languageOptions: {
+    globals: globals.browser,
+    parserOptions: {
+      ecmaFeatures: { jsx: true },
     },
   },
-])
+  settings: {
+    react: { version: 'detect' },
+  },
+  rules: {
+    'semi': ['error', 'never'],
+    'max-len': ['error', { code: 120 }],
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+  },
+}, ...storybook.configs["flat/recommended"]];
